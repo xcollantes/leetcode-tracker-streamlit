c58cb8ae1d801aab130f05158decc0d901391e51
Xavier <collantes.xavier@gmail.com>
2024-11-18 21:09:18 -0800

    Add new users
    Signed-off-by: Xavier <collantes.xavier@gmail.com>

---
+++ pages/Xavier.py
@@ -0,0 +1,14 @@
+import logging
+
+from modules.user import user_template
+
+logging.basicConfig(level=logging.DEBUG, format="%(message)s")
+
+
+def main():
+    TAB_NAME: str = "Xavier"
+    user_template(TAB_NAME)
+
+
+if __name__ == "__main__":
+    main()
